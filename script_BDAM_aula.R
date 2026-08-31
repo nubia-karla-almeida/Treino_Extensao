f9b43a37745b9e7e08df2e39c0f2ad0885441c8a
nubia-karla-almeida <nubia_karla@id.uff.br>
2026-08-31 10:44:22 -0300

    script - tarefas 1 a 3

--- script_BDAM_aula.R
+++ script_BDAM_aula.R
@@ -61,6 +61,106 @@ dados_bd1$F_IDADE = ifelse(dados_bd1$IDADE_CONDUTOR_CAUSADOR < 35, "22 a 34", "3
 # TADA: total de acidentes cuja causa foi o uso de drogas ou álcool
 # TACO: total de acidentes cuja causa foi outros
 
+names(dados_bd1)[names(dados_bd1) == "MUNICIPIO"] = "CODIGO"
+
+base = data.frame(CODIGO = sort(unique(dados_bd1$CODIGO)))
+
+# TV: total de veículos causadores de acidentes
+TV = as.data.frame(table(factor(dados_bd1$CODIGO)))
+names(TV) = c("CODIGO","TV")
+
+base = merge(base, TV, by = "CODIGO", all.x = TRUE)
+
+
+# TC: total de carros causadores do acidente e TM: total de motos causadoras do acidente
+tab = table(dados_bd1$CODIGO, factor(dados_bd1$VEICULO_CAUSADOR, levels = c("Carro","Moto")))
+df = as.data.frame.matrix(tab)
+names(df) = c("TC","TM")
+df$CODIGO = rownames(df)
+
+base = merge(base, df, by = "CODIGO", all.x = TRUE)
+
+# TVCF: total de veículos causadores de acidentes com condutor mulher e TVCM: total de veículos causadores de acidentes com condutor homem
+tab = table(dados_bd1$CODIGO, factor(dados_bd1$SEXO_CONDUTOR_CAUSADOR, levels = c("Feminino","Masculino")))
+df = as.data.frame.matrix(tab)
+names(df) = c("TVCF","TVCM")
+df$CODIGO = rownames(df)
+
+base = merge(base, df, by = "CODIGO", all.x = TRUE)
+
+# TC_22_34: total de condutores causadores de acidentes na faixa etária de 22 a 34 anos e TC_35_45: total de condutores causadores de acidentes na faixa etária de 35 a 45 anos
+tab = table(dados_bd1$CODIGO, factor(dados_bd1$F_IDADE, levels = c("22 a 34","35 a 45")))
+df = as.data.frame.matrix(tab)
+names(df) = c("TC_22_34","TC_35_45")
+df$CODIGO = rownames(df)
+
+base = merge(base, df, by = "CODIGO", all.x = TRUE)
+
+# NMF: número médio de feridos e DPF: desvio-padrão de feridos
+media_feridos = aggregate(NUM_FERIDOS_GRAVES ~ CODIGO, dados_bd1, mean, na.rm = TRUE)
+media_feridos$NUM_FERIDOS_GRAVES = round(media_feridos$NUM_FERIDOS_GRAVES, 2)
+names(media_feridos)[2] = "NMF"
+
+
+dp_feridos = aggregate(NUM_FERIDOS_GRAVES ~ CODIGO, dados_bd1, sd, na.rm = TRUE)
+dp_feridos$NUM_FERIDOS_GRAVES = round(dp_feridos$NUM_FERIDOS_GRAVES, 2)
+names(dp_feridos)[2] = "DPF"
+
+temp = merge(media_feridos, dp_feridos, by = "CODIGO")
+base = merge(base, temp, by = "CODIGO", all.x = TRUE)
+
+# F_P25: percentil 25 do número de feridos F_P50: percentil 50 do número de feridos e F_P75: percentil 75 do número de feridos
+p_feridos = aggregate(NUM_FERIDOS_GRAVES ~ CODIGO,dados_bd1, function(x) quantile(x, probs = c(0.25,0.5,0.75), na.rm = TRUE))
+p_feridos = do.call(data.frame, p_feridos)
+names(p_feridos) = c("CODIGO","F_P25","F_P50","F_P75")
+p_feridos[, c("F_P25","F_P50","F_P75")] = round(p_feridos[, c("F_P25","F_P50","F_P75")], 2)
+
+base = merge(base, p_feridos, by="CODIGO", all.x=TRUE)
+
+# TAFA: total de acidentes cuja causa foi falta de atenção  TADS: total de acidentes cuja causa foi desrespeito à sinalização
+# TADA: total de acidentes cuja causa foi o uso de drogas ou álcool  TACO: total de acidentes cuja causa foi outros
+tab = table(dados_bd1$CODIGO, factor(dados_bd1$CAUSA_ACIDENTE, levels = c("falta de atencao","desrespeito a sinalizacao", "alcool ou drogas", "outros")))
+df = as.data.frame.matrix(tab)
+names(df) = c("TAFA","TADS", "TADA", "TACO")
+df$CODIGO = rownames(df)
+
+base = merge(base, df, by = "CODIGO", all.x = TRUE)
+
+# Linha da UF
+linha_estado = base[1, ]
+linha_estado[,] = NA
+
+# Colunas de contagem: indicar as variáveis contínuas, que por exclusão não terão valores somados
+cols_contagem = setdiff(names(base), c("CODIGO","NMF","DPF","F_P25","F_P50","F_P75"))
+
+linha_estado[cols_contagem] = colSums(base[cols_contagem], na.rm = TRUE)
+
+# Colunas de medidas para variáveis quantitativas 
+linha_estado$NMF = round(mean(dados_bd1$NUM_FERIDOS_GRAVES, na.rm = TRUE), 2)
+linha_estado$DPF = round(sd(dados_bd1$NUM_FERIDOS_GRAVES, na.rm = TRUE), 2)
+
+q = round(quantile(dados_bd1$NUM_FERIDOS_GRAVES, probs = c(0.25,0.5,0.75), na.rm = TRUE), 2)
+linha_estado$F_P25 = q[1]
+linha_estado$F_P50 = q[2]
+linha_estado$F_P75 = q[3]
+
+# Código da UF e ordem das colunas
+linha_estado$CODIGO = 33
+
+# Banco de dados final para o Rio de Janeiro
+BANCO1_RJ = rbind(linha_estado, base)
+
+BANCO1_RJ$NIVEL = c("UF", rep("MUNICIPIO", nrow(BANCO1_RJ)-1))
+BANCO1_RJ$ANO = 2025
+
+BANCO1_RJ = BANCO1_RJ[, c("ANO","NIVEL","CODIGO", names(BANCO1_RJ)[!names(BANCO1_RJ) %in% c("ANO","NIVEL","CODIGO")])]
+BANCO1_RJ$CODIGO = as.character(BANCO1_RJ$CODIGO)
+
+# Verificando o banco final
+str(BANCO1_RJ)
+head(BANCO1_RJ)
+dim(BANCO1_RJ)
+
 # Ao terminar a Tarefa 3 commit com a mensagem " script - tarefa 1 a 3" e envie para o repositório Treino_Extensao
 
 
